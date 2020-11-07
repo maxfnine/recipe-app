94d5f2f97ce503e05b25d900806ed63423a199e5
maxfnine <maxfnine@gmail.com>
2020-11-07 16:57:22 +0200

    Added Boostrap in head

--- src/main/java/guru/springframework/recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/recipeapp/bootstrap/RecipeBootstrap.java
@@ -4,15 +4,19 @@ import guru.springframework.recipeapp.domain.*;
 import guru.springframework.recipeapp.repositories.CategoryRepository;
 import guru.springframework.recipeapp.repositories.RecipeRepository;
 import guru.springframework.recipeapp.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.data.annotation.Transient;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -27,6 +31,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
     }
@@ -196,6 +201,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         tacosRecipe.getCategories().add(mexicanCategory);
 
         recipes.add(tacosRecipe);
+        log.debug("Recipes created = {}",recipes);
         return recipes;
     }
 }
--- src/main/java/guru/springframework/recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package guru.springframework.recipeapp.services;
 
 import guru.springframework.recipeapp.domain.Recipe;
 import guru.springframework.recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -17,6 +19,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in service");
         HashSet<Recipe> recipesSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipesSet::add);
         return recipesSet;
