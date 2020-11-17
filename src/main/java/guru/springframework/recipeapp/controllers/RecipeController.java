e48ccf849789e6a3f06bd69ed090cb296e9a18d3
maxfnine <maxfnine@gmail.com>
2020-11-17 14:01:06 +0200

    Added NotFoundException 404error view and exception handling

--- src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
@@ -1,11 +1,16 @@
 package guru.springframework.recipeapp.controllers;
 
 import guru.springframework.recipeapp.commands.RecipeCommand;
+import guru.springframework.recipeapp.exceptions.NotFoundException;
 import guru.springframework.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 @RequestMapping("/recipe")
 public class RecipeController {
@@ -45,6 +50,14 @@ public class RecipeController {
         return "redirect:/";
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling not found exception");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        return modelAndView;
+    }
 
 
 }
